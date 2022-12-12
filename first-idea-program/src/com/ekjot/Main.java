2759bc6cd7104c1890c9d2301f3f522cabf5f177
Ekjot Singh <workforekjot@gmail.com>
2022-12-12 22:46:28 +0530

    Basic Operations in Java

--- first-idea-program/src/com/ekjot/Main.java
+++ first-idea-program/src/com/ekjot/Main.java
@@ -1,7 +1,15 @@
 package com.ekjot;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.print(34567);
+        System.out.println(34567);
         System.out.println("Hello World!!");
+        Scanner input = new Scanner(System.in);
+        Scanner input1 = new Scanner(System.in);
+        System.out.println(input.nextInt());
+        System.out.println(input1.nextLine());
     }
 }
